5ca15323a2ce36749333ffc433dd0a4fddbde03b
Maksim Izofatov <giantsgnom@gmail.com>
2022-12-10 23:44:18 +0300

    Home task 1

--- app/src/main/java/com/example/firstappizofatov/MainActivity.kt
+++ app/src/main/java/com/example/firstappizofatov/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.firstappizofatov
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txt = findViewById<TextView>(R.id.textView1)
+        if(android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.N){
+            txt.setTextColor(R.color.blue.toInt())
+            //findViewById<TextView>(R.id.textView1).setTextColor(resources.getColor(R.color.blue))
+        }
+        else
+        {
+            txt.setTextColor(R.color.red.toInt())
+        }
     }
+
 }
